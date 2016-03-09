fdf34f1954a63dfd944ca23a3c081e4f16da9f86
pablomr96 <pablomr960@gmail.com>
2016-03-09 12:22:53 +0100

    cipote

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "rutinas_clock.h"
 
 // R = 0.5*512, 1.5*512, 0.5*512, 0.75*512, 2*512, 4*512, 8*512
-#define R 1000
 // D = 2, 12, 26, 64, 92
-#define D 2
 
-int main()
-{
+int main(int argc,char *argv[]){
+	int D=atoi(argv[1]);
+	int L=512*atoll(argv[2]);
+	int R=(int)((L/D)+1);
 	double *A = NULL;
 	int e[10*R];
 	double S[10];
@@ -15,7 +15,7 @@ int main()
 	A = _mm_malloc(R*D*8,512);
 	int i=0;
 	for(i=0; i<R*D;i++)
-		*(A+i)=rand();
+		*(A+i)=((double)rand()/(double)RAND_MAX);
 
 	i=0;
 	for(i=0;i<10*R;i++)
@@ -23,18 +23,18 @@ int main()
 
 	i=0;
 	int j=0;
+	start_counter();
 	for(i=0;i<10;i++){
-		start_counter();
 		double sum = 0;
 		for(j=0;j<R*D;j++){
 			sum+=*(A+e[i]);
 		}
-		ck=get_counter();
-		mhz(1, 1);
-		printf("\nClocks=%1.10lf \n", ck);
 		total_ck+=ck;
 		S[i]=sum;
 	}
+	ck=get_counter();
+	printf("\nClocks=%1.10lf \n", ck);
+	mhz(1, 1);
 
 	for(i=0;i<10;i++)
 		printf("Resultado %d: %f\n", i+1,S[i]);
