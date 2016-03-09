167e28cd7b6e8e1443da7116afbdaf63ed56d590
pablomr96 <pablomr960@gmail.com>
2016-03-09 12:57:50 +0100

    main.c

--- main.c
+++ main.c
@@ -6,28 +6,29 @@
 int main(int argc,char *argv[]){
 	int D=atoi(argv[1]);
 	int L=512*atoll(argv[2]);
-	int R=(int)((L/D)+1);
+	int R=(int)((L*8/D)+1);
 	double *A = NULL;
 	int e[10*R];
 	double S[10];
 	double ck=0,total_ck=0;
 
 	A = _mm_malloc(R*D*8,512);
-	int i=0;
+	int i=0,j=0,k=0;
 	for(i=0; i<R*D;i++)
 		*(A+i)=((double)rand()/(double)RAND_MAX);
 
-	i=0;
-	for(i=0;i<10*R;i++)
-		e[i]=i*D;
+	e[0]=0;
+	for(i=1;i<10*R;i++){
+		if(i%10==0)
+			k+=1;
+		e[i]=k*D;
+	}
 
-	i=0;
-	int j=0;
 	start_counter();
 	for(i=0;i<10;i++){
 		double sum = 0;
 		for(j=0;j<R*D;j++){
-			sum+=*(A+e[i]);
+			sum+=*(A+e[j]);
 		}
 		total_ck+=ck;
 		S[i]=sum;
