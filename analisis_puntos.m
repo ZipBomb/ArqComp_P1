e3299ddcf5a17f9c13e797a96d41b7e463c9a960
pablomr96 <pablomr960@gmail.com>
2016-03-15 12:46:18 +0100

    Added files via upload

--- analisis_puntos.m
+++ analisis_puntos.m
@@ -6,8 +6,20 @@ close all
 t=load('puntos.txt'); 
 x=t(:,1);
 y=t(:,2);
-
-plot(x,y,'-r*','linewidth',2,'markersize',6,'markeredgecolor','k','markerfacecolor','k');
+x1=[0:(length(x)/3)-1];
+y1=[0:(length(y)/3)-1];
+k=1;
+i=3;
+while i<=length(x)
+  z=x(i)*x(i-1)*x(i-2);
+  x1(k)=z^(1/3);
+  y1(k)=y(i);
+  i=i+3;
+  k=k+1;
+endwhile
+x1
+y1
+plot(x1,y1,'-r*','linewidth',2,'markersize',6,'markeredgecolor','k','markerfacecolor','k');
 title('Analisis de los Resultados');
 xlabel('Numero Total de Lineas Cache');
 ylabel('Coste Medio por Iteracion');
